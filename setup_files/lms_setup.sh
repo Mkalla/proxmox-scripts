d838ee6de24dccbbbda4374c24daf3e923369e52
Mkalla <Mkalla@users.noreply.github.com>
2024-12-04 18:28:15 +0100

    Update lms_setup.sh

--- setup_files/lms_setup.sh
+++ setup_files/lms_setup.sh
@@ -41,10 +41,10 @@ apt -qqy install \
     gnupg &>/dev/null
     
 echo -e "${CHECKMARK} \e[1;92m Downloading Logitech Media Server... \e[0m"
-wget https://downloads.slimdevices.com/LogitechMediaServer_v8.2.0/logitechmediaserver_8.2.0_amd64.deb &>/dev/null
+wget https://downloads.lms-community.org/LyrionMusicServer_v9.0.0/lyrionmusicserver_9.0.0_amd64.deb &>/dev/null
 
 echo -e "${CHECKMARK} \e[1;92m Installing Logitech Media Server... \e[0m"
-sudo apt install -y ./logitechmediaserver_8.2.0_amd64.deb  &>/dev/null
+sudo apt install -y ./lyrionmusicserver_9.0.0_amd64.deb  &>/dev/null
 
 echo -e "${CHECKMARK} \e[1;92m Customizing Container... \e[0m"
 rm /etc/motd 
